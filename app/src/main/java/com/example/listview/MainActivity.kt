5c7cf59f09a94c110f9882b627b8e3eea600ab2a
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-05 22:14:23 -0500

    ListView con adaptador personalizado

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 
@@ -14,20 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var frutas:ArrayList<String> = ArrayList(0)
-        frutas.add("Manzana")
-        frutas.add("Durazno")
-        frutas.add("Plátano")
-        frutas.add("Sandía")
+        var frutas:ArrayList<Fruta> = ArrayList(0)
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Durazno", R.drawable.durazno))
+        frutas.add(Fruta("Plátano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
 
         val lista = findViewById<ListView>(R.id.lista)
 
-        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+       // val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+        val adaptador = AdaptadorCustom(this, frutas)
 
         lista.adapter = adaptador
 
         lista.onItemClickListener = AdapterView.OnItemClickListener { parent, view,position, id ->
-            Toast.makeText(this, frutas.get(position),Toast.LENGTH_LONG).show()
+            Toast.makeText(this, frutas.get(position).nombre,Toast.LENGTH_LONG).show()
         }
 
     }
